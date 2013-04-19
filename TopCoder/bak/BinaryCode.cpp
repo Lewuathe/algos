0733f0724763289f676fb84dc4616e39a273ff7e
Lewuathe <lewuathe@me.com>
2013-04-19 23:56:28 +0900

    Reform TopCoder environment

--- TopCoder/bak/BinaryCode.cpp
+++ TopCoder/bak/BinaryCode.cpp
@@ -20,8 +20,3 @@ class BinaryCode {
 // Powered by FileEdit
 // Powered by TZTester 1.01 [25-Feb-2003]
 // Powered by CodeProcessor
-
-
-// Powered by FileEdit
-// Powered by TZTester 1.01 [25-Feb-2003]
-// Powered by CodeProcessor
