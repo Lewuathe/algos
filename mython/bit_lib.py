4f1370770327892c0b5e1ed1b706b6657cab0d89
Lewuathe <lewuathe@me.com>
2014-02-22 23:53:51 +0900

    Add bit_lib

---
+++ mython/bit_lib.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+def get_bit(num, i):
+    if num & (1 << i) != 0:
+        return 1
+    else:
+        return 0
+
+def set_bit(num, i):
+    return num | (1 << i)
+
+def clear_bit(num, i):
+    mask = ~(1 << i)
+    return num & mask
+
+if __name__ == "__main__":
+    print format(10, 'b')
+    print get_bit(10, 1)
+    print format(set_bit(10, 2), 'b')
+    print format(clear_bit(10, 1), 'b')
